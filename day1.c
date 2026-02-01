2beeb7425cde7c698a8d39d1c11efa3262cdecd0
Shaurya Sood <shauryasood008@gmail.com>
2026-02-01 23:24:53 +0530

    Update day1.c

--- day1.c
+++ day1.c
@@ -27,37 +27,24 @@ Explanation: Insert 3 at position 3, elements [4,5,6] shift right*/
 
 int main() {
     int n, pos, x;
-    int arr[100];   // assuming max size
-
-    // Input size
+    int arr[100];   
     scanf("%d", &n);
-
-    // Input array elements
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
-    // Input position (1-based)
     scanf("%d", &pos);
-
-    // Input element to insert
     scanf("%d", &x);
-
-    // Shift elements to the right
     for (int i = n; i >= pos; i--) {
         arr[i] = arr[i - 1];
     }
-
-    // Insert the element
     arr[pos - 1] = x;
-
-    // Print updated array
     for (int i = 0; i <= n; i++) {
         printf("%d ", arr[i]);
     }
 
     return 0;
 }
+
 
 
 
